35101003da221fbd0c736e52d9007d3a895ecfb0
Rosie Val <everestmcarthur@gmail.com>
2026-08-22 21:21:05 -0400

    fix(build): compile bundles to Hermes bytecode by default

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -24,6 +24,7 @@ const {
     "build-minify": buildMinify,
     "dev": dev,
     "target": buildTarget = "old",
+    "skip-hermes": skipHermes = false,
 } = args;
 
 if (!["old", "new"].includes(buildTarget)) {
@@ -165,7 +166,8 @@ const pathPassedToNode = path.resolve(process.argv[1]);
 const isThisFileBeingRunViaCLI = pathToThisFile.includes(pathPassedToNode);
 
 if (isThisFileBeingRunViaCLI) {
-    const { timeTook } = await buildBundle({}, true);
+    const compileHermes = !skipHermes;
+    const { timeTook } = await buildBundle({}, compileHermes);
 
     printBuildSuccess(
         context.hash,
@@ -177,7 +179,7 @@ if (isThisFileBeingRunViaCLI) {
         const { timeTook } = await buildBundle({
             minify: true,
             outfile: config.outfile.replace(/\.js$/, ".min.js")
-        }, true);
+        }, compileHermes);
 
         printBuildSuccess(
             context.hash,
